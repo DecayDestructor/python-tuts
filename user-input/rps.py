85d8d4f2f95b0e8324d869e57f33dbda2122aa61
Decay_Destructor <aaryanmantri29@gmail.com>
2024-07-03 12:30:47 +0530

    user-input

--- user-input/rps.py
+++ user-input/rps.py
@@ -1,14 +1,28 @@
 import sys
 import random
+from enum import Enum
+
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+
+# print(RPS(1))
+# print(RPS.ROCK)
+# print(RPS["ROCK"])
+# print(RPS.ROCK.value)
+
 
 print("")
 
 
-playerChoice = input("Enter...\n1. Rock \n2.Paper \n3.Scissors\n")
+playerChoice = input("Enter...\n1.Rock \n2.Paper \n3.Scissors\n")
 
 player = int(playerChoice)
 
-if player < 1 | player > 3:
+if player < 1 or player > 3:
     sys.exit("Enter a valid choice")
 
 computerChoice = random.choice("123")
@@ -16,7 +30,11 @@ computerChoice = random.choice("123")
 computer = int(computerChoice)
 
 print(
-    "\nYou chose : " + playerChoice + "\nComputer choice is : " + computerChoice + "\n"
+    "\nYou chose : "
+    + str(RPS(player)).replace("RPS.", "")
+    + "\nComputer choice is : "
+    + str(RPS(computer)).replace("RPS.", "")
+    + "\n"
 )
 
 if player == 1 and computer == 3:
@@ -27,6 +45,9 @@ elif player == 2 and computer == 1:
 
 elif player == 3 and computer == 2:
     print("Player wins!")
+
+elif player == computer:
+    print("It's a tie!")
 
 else:
     print("Computer wins!")
